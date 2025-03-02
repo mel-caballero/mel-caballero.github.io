a148fb4b8211ab70339d2c47ed6f8c991c68d59d
Melanie Caballero <melanie.caballero.pastor@gmail.com>
2025-03-02 19:38:22 +0100

    changes

---
+++ data/data.js
@@ -0,0 +1,8 @@
+const temas = {
+  "tema": [
+    { "titulo": "Sobre mi", "ruta": "assets/", "nombre": "sobre-mi" },
+    { "titulo": "Mis 3 pilares", "ruta": "assets/", "nombre": "mis-3-pilares" },
+    { "titulo": "Proyectos", "ruta": "assets/", "nombre": "proyectos" },
+    { "titulo": "Eventos", "ruta": "assets/", "nombre": "eventos" },
+  ]
+};
